089b738ae16a11d35f7c789d6c6d09e5d4320693
Aliahmad565 <131805641+Aliahmad565@users.noreply.github.com>
2023-05-13 15:18:17 -0700

    Add files via upload

---
+++ program.js
@@ -0,0 +1,5 @@
+hamburger = document.querySelector(".bar");
+    hamburger.onclick = function(){
+        navBar = document.querySelector(".nav-bar");
+        navBar.classList.toggle("active");
+    }
